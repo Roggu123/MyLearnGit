9a082ac56c2743831e339a9136759a954238f679
Frank <2946994766@qq.com>
2018-12-29 18:01:31 +0800

    The first try of update repository

---
+++ StudyGit/StudyGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyGit
+//
+//  Created by Ruogu Lu on 2018/12/29.
+//  Copyright © 2018 Ruogu Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
